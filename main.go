7cc53cd3ffeba69aeabe71aa7fc6ee1540420777
unknown <sahatmarbun147@gmail.com>
2022-02-18 15:02:12 +0700

    connect to database

--- main.go
+++ main.go
@@ -4,12 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golangApi.test/models"
 )
 
 func main() {
+
 	r := gin.Default()
+
+	//Models
+
+	db := models.Setup()
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "Go Api"})
 	})
+
+	r.Run(":8080")
 
 }
--- models/product.go
+++ models/product.go
@@ -1,6 +1,7 @@
 package models
 
 type Products struct {
+	Id    int     `json:"id"`
 	Code  string  `json:"code"`
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
